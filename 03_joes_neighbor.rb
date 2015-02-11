52d3316e2259ab3288aba035b9ef4f78fde2be96
Daniel Harris <bobth3bum@gmail.com>
2015-02-11 09:39:04 -0700

    finish 3rd exercise

--- 03_joes_neighbor.rb
+++ 03_joes_neighbor.rb
@@ -3,3 +3,7 @@ require_relative 'people'
 # What is the last name of the person sitting next to joe?
 #
 # HINT: Start by finding who joe is sitting next to, then find that person in the hash.
+
+@next_to_joe = PEOPLE["joe"][:sitting_next_to]
+
+puts PEOPLE[@next_to_joe.to_s][:last_name]
